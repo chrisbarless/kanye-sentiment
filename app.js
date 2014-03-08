975153b0ae1b3e882d75dc2c5aa9ef2132505a61
chrisbarless <chrisbarless@gmail.com>
2014-03-08 17:38:03 -0500

    Boilerplate added

--- app.js
+++ app.js
@@ -7,6 +7,6 @@ var debug = require('debug')('app'),
 require('./lib/router')(server, handler);
 
 // All set, start listening!
-var port = 3000;
+var port = 5000;
 server.listen(port);
 debug("Express server listening on port %d in %s mode", port, process.env.NODE_ENV);
