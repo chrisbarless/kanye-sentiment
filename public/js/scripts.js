11c19fdbe8ac0524d57163088ee5b6f798253fdb
chrisbarless <chrisbarless@gmail.com>
2014-03-09 16:49:17 -0400

    background animation

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -7,23 +7,56 @@
 			width: $(window).width(),
 			height: $(window).height()
 		};
-		this.setMood = function(mood){
-			if (mood === 0) mood = 1;
-			mood = Math.abs(mood - 100);
-			var frame = Math.floor(mood / 2),
-					offset = -frame * wSize.width;
-			$el.css('background-position-x', offset)
-			console.debug('Frame set to %d', frame);
-			console.debug('Offset set to %d', offset);
-			return true;
-		};
+    this._mood = 1;
+    this.setMood = function(mood){
+      if (mood === 0) mood = 1;
+      this._animate(this._mood, mood);
+      this._mood = mood;
+      console.debug('Mood set to %d', mood);
+      return true;
+    };
+    this._animate = function(start, stop){
+      if (start === stop) return false;
+      var direction = start > stop ? 'up' : 'down';
+      var gap = (direction === 'up' ? start - stop : stop - start);
+      var klass = this;
+      var delay = 0;
+      gap = gap / 10;
+      if (direction === 'down'){
+        for (var i = start; i < stop; i += gap) {
+          i = Math.floor(i);
+          setTimeout(function(mood){
+            klass._setFrame(mood)
+          }, delay, i);
+          delay += 100;
+        };
+      } else {
+        for (var i = start; i > stop; i -= gap) {
+          i = Math.floor(i);
+          setTimeout(function(mood){
+            klass._setFrame(mood)
+          }, delay, i);
+          delay += 100;
+        };
+      }
+    };
+    this._setFrame = function(mood){
+      var frame;
+      frame = Math.abs(mood - 100) / 2;
+      frame = Math.floor(frame);
+      var offset = -frame * wSize.width;
+      $el.css('background-position-x', offset);
+      console.debug('Offset set to %d', offset);
+      return true;
+    };
 		this.init = function(){
 			var str = wSize.width * 50 + 'px ' + wSize.height + 'px';
 			$el.css('background-size', str);
-			this.setMood(25);
+      this.setMood(1);
 			return true;
 		};
-		return this.init();
+    this.init();
+		return true;
 	};
 
 	$(function(){
