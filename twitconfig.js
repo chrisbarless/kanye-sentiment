622a306ad687d7e782d463a6cb1d89f849aeded0
chrisbarless <chrisbarless@gmail.com>
2014-03-08 20:14:40 -0500

    Removing config file

--- twitconfig.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-  consumer_key:         'EDBivaJsQE7EI9dbAow',
-  consumer_secret:      'mr8cOc3KwqIzZt2hZeshzJa5lpSTKXHFgRfDEBjbM',
-  access_token:         '260941979-XDzDoSgYtYZD4bQirmpU6gDG2nhq2ehC7JccWxLj',
-  access_token_secret:  'VQQNReA9tVkAsFVVOMoU4D4ZhPUOhIlla5LHZuV8LIVbG'
-};
