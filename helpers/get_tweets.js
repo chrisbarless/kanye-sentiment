5d4cc0073b084c2e43037bcbfdc9515f091e8989
chrisbarless <chrisbarless@gmail.com>
2014-03-11 21:29:53 -0400

    Trying out some new environment variables

--- helpers/get_tweets.js
+++ helpers/get_tweets.js
@@ -4,7 +4,12 @@ var fs = require('fs'),
   tweetFile = 'tweets.txt',
   encoding = 'utf8';
 
-var T = new Twit(process.cwd() + '/twitconfig');
+var T = new Twit({
+    consumer_key: process.env.CONSUMER_KEY,
+    consumer_secret: process.env.CONSUMER_SECRET,
+    access_token: process.env.ACCESS_TOKEN,
+    access_token_secret: process.env.ACCESS_TOKEN_SECRET
+});
 var writeTweets = function(){
   var searchParams = {q: "kanye", count: 100, result_type: "mixed"};
   T.get('search/tweets', searchParams, function(err, data) {
