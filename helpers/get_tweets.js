d4f9821deaa28f4b1c8aa1c87e0e96476a37e331
chrisbarless <chrisbarless@gmail.com>
2014-03-12 00:20:34 -0400

    new tweets

--- helpers/get_tweets.js
+++ helpers/get_tweets.js
@@ -4,25 +4,27 @@ var fs = require('fs'),
   tweetFile = 'tweets.txt',
   encoding = 'utf8';
 
+var oldTweets = fs.readFileSync(tweetFile);
+oldTweets = JSON.parse(oldTweets);
+
 var T = new Twit(require('../twitconfig'));
+
 var writeTweets = function(){
   var searchParams = {q: "kanye", count: 100, result_type: "mixed"};
   T.get('search/tweets', searchParams, function(err, data) {
-    var tweets;
+    var newTweets, tweets;
     if (err) throw err;
     if (data.statuses.length === 0) return false;
-    tweets = _.chain(data.statuses)
-      .map(function(tweet){
-        return {
-          text: tweet.text,
-          user: tweet.user.screen_name
-        };
-      })
-      .uniq(function(tweet){
-        return tweet.text;
-      })
-      .value();
-    fs.appendFileSync(tweetFile, '\n' + JSON.stringify(tweets), { encoding: encoding });
+    newTweets = _.map(data.statuses, function(tweet){
+      return {
+        text: tweet.text,
+        user: tweet.user.screen_name
+      };
+    });
+    tweets = newTweets.uniq(oldTweets.concat(newTweets), function(tweet){
+      return tweet.text;
+    });
+    fs.writeFileSync(tweetFile, '\n' + JSON.stringify(tweets), { encoding: encoding });
     console.log('Wrote %d tweets', tweets.length);
   });
 };
