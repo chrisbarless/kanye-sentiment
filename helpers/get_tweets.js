afb9a70cd007ad25853dc90dfc1dfd1ee3046656
chrisbarless <chrisbarless@gmail.com>
2014-03-12 22:23:31 -0400

    Script test.

--- helpers/get_tweets.js
+++ helpers/get_tweets.js
@@ -12,7 +12,7 @@ var tweetConfig = process.env.ENVIRONMENT == 'production' ? {
    consumer_secret: process.env.CONSUMER_SECRET,
    access_token: process.env.ACCESS_TOKEN,
    access_token_secret: process.env.ACCESS_TOKEN_SECRET
-} || require('../twitconfig');
+} : require('../twitconfig');
 
 var T = new Twit(tweetConfig);
 
